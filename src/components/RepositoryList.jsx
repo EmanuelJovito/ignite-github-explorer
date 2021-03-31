ae5fbe50bea84cadcce59e26ecbe3b3c99742f2d
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-03-31 14:56:23 -0300

    feat: Iniciando a aplicaçao

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className='repository-list'>
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
+      </ul>
+    </section>
+  )
+}
